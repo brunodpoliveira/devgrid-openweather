09760dd34a314f7fba628d8a8f0d294b3f947034
bruno <brunodp.oliveira@gmail.com>
2021-01-22 00:09:03 -0300

    Update main.py

--- test-algos/main.py
+++ test-algos/main.py
@@ -1,8 +1,7 @@
 import pyowm
 import json
-import time
+import datetime
 import requests
-from pprint import pprint
 
 # ------------------------------------------------------------------------
 
@@ -12,48 +11,49 @@ with open('api_key.json', 'r') as file:
 api_key = creds['API_KEY']
 # instantiate object
 owm_obj = pyowm.OWM(api_key)
+# time converter
+d = {'date': datetime.datetime.now()}
+
+
+def timeconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+
 # ------------------------------------------------------------------------
-#user_id = input("user id: ")
+user_id = input("user id: ")
 
 city_name = input("city name : ")
 
 # base_url + api_key + city_name = final url
-final_url = creds['BASE_URL'] + "appid=" + api_key + "&q" + city_name
+final_url = creds['BASE_URL'] + "q=" + city_name + "&appid=" + api_key
 
 # capture own api json data
 w_data = requests.get(final_url).json()
-pprint(w_data)
 # ------------------------------------------------------------------------
-#city_id = w_data["id"]
-#temp = w_data('celsius')["temp"]
-#humidity = w_data["humidity"]
-#print('temp (celsius)' + str(temp) + 'city id' + str(city_id) + 'humidity' + str(humidity))
-# ------------------------------------------------------------------------
-"""
+
 if w_data["cod"] != "404":
     a = w_data
-    b = w_data["weather"]
+    city_id = a["id"]
+    temp = [a['main']['temp']]
+    humidity = [a['main']['humidity']]
 
-    print('temp (celsius)' + str(temp) + 'city id' + str(city_id) + 'humidity' + str(humidity))
+    print('temp (celsius) ' + str(temp) + 'city id ' + str(city_id) + 'humidity ' + str(humidity))
 
 else:
     print('not found')
 # ------------------------------------------------------------------------
+with open("result.json", mode='r', encoding='utf-8') as f:
+    f_result = json.load(f)
+with open("result.json", mode='w', encoding='utf-8') as f:
+    entry = {'CITY_ID': city_id, 'TEMPERATURE': temp, 'HUMIDITY': humidity}
+    f_result.append(entry)
+    json.dump(f_result, f)
 
-# TODO: STORE UNIQUE USER ID AND Datetime of request
-u_id_json = {}
-u_id_json['USER_ID'] = user_id
-u_id_json['REQUEST_DATETIME'] = ""
+with open("user_id.json", mode='r', encoding='utf-8') as f:
+    f2_result = json.load(f)
+with open("user_id.json", mode='w', encoding='utf-8') as f2:
+    entry = {'USER_ID': user_id, 'REQUEST_DATETIME': json.dumps(d, default=timeconverter)}
+    f2_result.append(entry)
+    json.dump(f2_result, f2)
 
-# ------------------------------------------------------------------------
-
-s_result = {}
-s_result['CITY_ID'] = city_id
-s_result['TEMPERATURE'] = temp
-s_result['HUMIDITY'] = humidity
-
-with open("result.json", "w") as file:
-    json.dump(s_result, file)
-with open("user_id.json", "w") as file2:
-    json.dump(u_id_json, file2)
-"""
